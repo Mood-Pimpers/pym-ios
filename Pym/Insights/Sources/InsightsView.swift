2666b087f9209fe0b0039060a57aaf3c82da6424
Simon Ammer <ammersimon@gmail.com>
2020-12-30 15:38:15 +0100

    Add InsightsView

---
+++ Pym/Insights/Sources/InsightsView.swift
@@ -0,0 +1,21 @@
+//
+//  Insights.swift
+//  Insights
+//
+//  Created by Simon Ammer on 21.12.20.
+//
+
+import SwiftUI
+
+struct InsightsView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct InsightsView_Previews: PreviewProvider {
+    static var previews: some View {
+        InsightsView()
+    }
+}
