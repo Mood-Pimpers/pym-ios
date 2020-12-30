216306d9d3acde35a186b334b84fd789da7d5d89
Simon Ammer <ammersimon@gmail.com>
2020-12-30 10:35:21 +0100

    Refactor project structure that the xcodeproj file is in the root directory

--- Pym/Explorer/Tests/ExplorerTests.swift
+++ Pym/Explorer/Tests/ExplorerTests.swift
@@ -5,11 +5,10 @@
 //  Created by Simon Ammer on 30.12.20.
 //
 
-import XCTest
 @testable import Explorer
+import XCTest
 
 class ExplorerTests: XCTestCase {
-
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -25,9 +24,8 @@ class ExplorerTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
