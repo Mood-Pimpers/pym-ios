28e80fb06d75718adb172a9f1f4b799c678e6d68
Simon Ammer <ammersimon@gmail.com>
2020-12-30 22:39:28 +0100

    #2 Add TabbView to root view

--- Pym/App/Sources/RootView.swift
+++ Pym/App/Sources/RootView.swift
@@ -5,12 +5,53 @@
 //  Created by Simon Ammer on 21.12.20.
 //
 
+import Explorer
+import Home
+import Insights
+import Settings
 import SwiftUI
 
 struct RootView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            GeometryReader { geometry in
+                TabView {
+                    HomeView()
+                        .tabItem {
+                            Image.home.colorMultiply(.blackColor)
+                        }
+                    InsightsView()
+                        .tabItem {
+                            Image.insights.renderingMode(.template).colorMultiply(.black)
+                        }
+                    Text("TODO: Add alert")
+                        .tabItem {}
+                    ExplorerView()
+                        .tabItem {
+                            Image.explorer.colorMultiply(.blackColor)
+                        }
+                    SettingsView()
+                        .tabItem {
+                            Image.settings.colorMultiply(.blackColor)
+                        }
+                }
+                .onAppear {
+                    UITabBar.appearance().barTintColor = Asset.whiteColor.color
+                }
+                .accentColor(.primaryColor)
+
+                ZStack {
+                    RoundedRectangle(cornerRadius: 14)
+                        .fill(Color.primaryColor)
+                        .frame(width: 60, height: 60)
+                        .shadow(color: Color.dropShadowColor, radius: 8, x: 0, y: 1)
+                    Image.add
+                        .resizable()
+                        .frame(width: 28, height: 28)
+                }
+                .offset(x: geometry.size.width / 2 - 30, y: geometry.size.height - 70)
+            }
+        }
     }
 }
 
---
+++ Pym/Explorer/Sources/ExplorerView.swift
@@ -0,0 +1,26 @@
+//
+//  ExplorerView.swift
+//  Explorer
+//
+//  Created by Simon Ammer on 30.12.20.
+//
+
+import PymCore
+import SwiftUI
+
+public struct ExplorerView: View {
+    public init() {}
+
+    public var body: some View {
+        ZStack {
+            Color.backgroundColor.edgesIgnoringSafeArea(.all)
+            Text("TODO: Explorer")
+        }
+    }
+}
+
+struct ExplorerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ExplorerView()
+    }
+}
