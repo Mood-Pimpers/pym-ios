1f7ff1997bab48777be0bc4873c690c0088c98c8
Manuel Fuchs <manuel.fuchs49@gmail.com>
2021-02-18 20:27:27 +0100

    add convenience initializer to moodentry

--- Pym/PymCore/Sources/Persistence/DTOs/MoodEntry.swift
+++ Pym/PymCore/Sources/Persistence/DTOs/MoodEntry.swift
@@ -1,17 +1,21 @@
 import Foundation
 
-public class MoodEntry: Identifiable {
-    public let id: UUID
+public class MoodEntry {
+    public var id: UUID?
     public let date: Date
     public let rating: MoodRating
     public let feelings: [Feeling]
     public let activities: [String]
 
-    init(id: UUID, date: Date, rating: MoodRating, feelings: [Feeling], activities: [String]) {
-        self.id = id
+    init(date: Date, rating: MoodRating, feelings: [Feeling], activities: [String]) {
         self.date = date
         self.rating = rating
         self.feelings = feelings
         self.activities = activities
+    }
+
+    convenience init(id: UUID, date: Date, rating: MoodRating, feelings: [Feeling], activities: [String]) {
+        self.init(date: date, rating: rating, feelings: feelings, activities: activities)
+        self.id = id
     }
 }
