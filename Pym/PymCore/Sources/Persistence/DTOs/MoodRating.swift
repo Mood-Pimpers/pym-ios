879bbb23d4b4b4ee6a655c53ec23d56e3c8739ee
Manuel Fuchs <manuel.fuchs49@gmail.com>
2021-02-20 19:22:13 +0100

    showing my dad some git stuff

--- Pym/PymCore/Sources/Persistence/DTOs/MoodRating.swift
+++ Pym/PymCore/Sources/Persistence/DTOs/MoodRating.swift
@@ -1,7 +1,7 @@
 public enum MoodRating: Int16 {
     case bad = 1
-    case poor
-    case moderate
-    case good
-    case great
+    case poor = 2
+    case moderate = 3
+    case good = 4
+    case great = 5
 }
