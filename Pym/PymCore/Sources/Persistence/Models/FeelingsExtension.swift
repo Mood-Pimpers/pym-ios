dfae6edfc91077b47eb3533c8e8811d0d49f6f8f
Manuel Fuchs <manuel.fuchs49@gmail.com>
2021-02-20 11:14:51 +0100

    splitup core data enum extensions

---
+++ Pym/PymCore/Sources/Persistence/Models/FeelingsExtension.swift
@@ -0,0 +1,8 @@
+//
+//  FeelingExtension.swift
+//  PymCore
+//
+//  Created by Manuel Fuchs on 20.02.21.
+//
+
+import Foundation
---
+++ Pym/PymCore/Sources/Persistence/Models/MoodRatingExtension.swift
@@ -0,0 +1,8 @@
+//
+//  MoodRatingExtension.swift
+//  PymCore
+//
+//  Created by Manuel Fuchs on 20.02.21.
+//
+
+import Foundation
