2502a7c010739ca9044811cc75e114e0a8750a29
Daniel Bauer <daniel.bauer.x@gmail.com>
2021-02-22 17:30:36 +0100

    Moved feeling color to CheckIn

---
+++ Pym/CheckIn/Sources/Extensions/Feeling.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+import PymCore
+
+public extension Feeling {
+    var color: Color {
+        switch self {
+        case .angry, .upset, .irritated: return Color.red
+        case .clear, .curious, .enthusiastic: return Color.orange
+        case .happy, .relaxed: return Color.yellow
+        case .scared, .anxious, .worried: return Color.green
+        case .amazed, .surprised, .confused: return Color.blue
+        case .distressed, .sad: return Color.purple
+        }
+    }
+}
--- Pym/PymCore/Sources/Persistence/DTOs/Feeling.swift
+++ Pym/PymCore/Sources/Persistence/DTOs/Feeling.swift
@@ -45,18 +45,3 @@ extension Feeling: CustomStringConvertible {
         }
     }
 }
-
-import SwiftUI
-
-public extension Feeling {
-    var color: Color {
-        switch self {
-        case .angry, .upset, .irritated: return Color.red
-        case .clear, .curious, .enthusiastic: return Color.orange
-        case .happy, .relaxed: return Color.yellow
-        case .scared, .anxious, .worried: return Color.green
-        case .amazed, .surprised, .confused: return Color.blue
-        case .distressed, .sad: return Color.purple
-        }
-    }
-}
