16723d265ae8ab59af835557e3fbb0d218b7883b
Daniel Bauer <daniel.bauer.x@gmail.com>
2021-02-23 14:17:23 +0100

    Add settings view

--- Pym/Settings/Sources/SettingsView.swift
+++ Pym/Settings/Sources/SettingsView.swift
@@ -2,12 +2,50 @@ import PymCore
 import SwiftUI
 
 public struct SettingsView: View {
+    @State private var text = ""
+    @State private var notify = true
+    @State private var onMorning = true
+    @State private var morning = Date()
+
     public init() {}
 
     public var body: some View {
-        ZStack {
-            Color.backgroundColor.edgesIgnoringSafeArea(.all)
-            Text("TODO: Settings")
+        NavigationView {
+            VStack(alignment: .leading, spacing: 0) {
+                List {
+                    Section(
+                        header: Text("Mood Tracking Schedule"),
+                        content: {
+                            Toggle("Get Notifications", isOn: $notify)
+
+                            if notify {
+                                HStack {
+                                    Toggle("Morning", isOn: $onMorning)
+                                        .labelsHidden()
+                                    DatePicker("Morning", selection: $morning, displayedComponents: .hourAndMinute)
+                                }
+
+                                HStack {
+                                    Toggle("Evening", isOn: $onMorning)
+                                        .labelsHidden()
+                                    DatePicker("Evening", selection: $morning, displayedComponents: .hourAndMinute)
+                                }
+                            }
+                        }
+                    )
+
+                    Section(
+                        header: Text("Data Cleanup"),
+                        content: {
+                            Button(action: {}, label: {
+                                Text("Erase my data")
+                            })
+                        }
+                    )
+                }
+                .listStyle(GroupedListStyle())
+            }
+            .navigationTitle("Settings")
         }
     }
 }
