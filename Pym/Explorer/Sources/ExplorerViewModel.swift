0b72e8d9f9ea86a885903ad316b2e2f4e395a268
Manuel Fuchs <manuel.fuchs49@gmail.com>
2021-02-27 23:38:44 +0100

    remove debug code ExplorerViewModel

--- Pym/Explorer/Sources/ExplorerViewModel.swift
+++ Pym/Explorer/Sources/ExplorerViewModel.swift
@@ -22,40 +22,25 @@ class ExplorerViewModel: ObservableObject {
 
     @Published var selectedDate: Date?
 
-    let calendarStartDate = /* DataAccessController.shared.getEarliestEntry()?.timestamp ?? Date() */ Date().adding(.month, value: -1)
+    let calendarStartDate = DataAccessController.shared.getEarliestEntry()?.timestamp ?? Date()
     let calendarEndDate = Date()
 
     func getMoodRatingsFor(day: Date) -> [MoodRating] {
-//        DataAccessController.shared
-//            .getEntries(fromDay: day)
-//            .map(\.rating)
-        switch day.day % 5 {
-        case 0:
-            return [.great, .good]
-        case 1:
-            return [.bad, .poor]
-        case 2:
-            return [.moderate]
-        default:
-            return []
-        }
+        DataAccessController.shared
+            .getEntries(fromDay: day)
+            .map(\.rating)
     }
 
     func getIncidents(forDay day: Date) -> [IncidentContainer] {
-        var incidents: [IncidentContainer] = /* [] */ DataAccessController.shared
+        var incidents: [IncidentContainer] = DataAccessController.shared
             .getEntries(fromDay: day)
             .map { IncidentContainer(incident: $0) }
-//        incidents.append(IncidentContainer(incident: MoodEntry(
-//            date: day,
-//            rating: MoodRating.allCases.randomElement()!,
-//            feelings: [.amazed, .clear],
-//            activities: ["University", "Work"]
-//        )))
 
         incidents.append(contentsOf: ExternalDataAccess.shared
             .getEvents(fromDay: day)
             .map { IncidentContainer(incident: $0) }
         )
+
         return incidents
     }
 }
